4ad0e1fd76e8ad8b49b04a68edd18c932aaee04d
Humaun Kabir <30367013+humaun-kabir@users.noreply.github.com>
2021-02-07 16:02:44 +0600

    Add files via upload

---
+++ problem5.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>problem5</title>
+</head>
+<body>
+		<?php
+			$backColor="green"; 
+			$fontColor="red";
+			$imageWidth="80px"; 
+			$imageHeight="80px";
+        ?>
+		<img src="aiublogo.png" width="<?php echo $imageWidth ?>" height="<?php echo $imageWidth ?>" />
+        <h2>American International University -<span style="background-color:<?php echo $backColor ?>;color:<?php echo $fontColor ?>">
+        Bangladesh</h2>
+        </span>
+</body>
+</html>
